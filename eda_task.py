b107d0f901ca15a6019ebd9a8145a71714193d27
SUNIL S <sunilsunil4277@gmail.com>
2026-05-02 04:09:09 +0530

    Add files via upload
    EDA Task Completed

---
+++ eda_task.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from data_profiling import ProfileReport
+
+# Load dataset
+df = pd.read_csv("data/Titanic-Dataset.csv")
+
+# Generate report
+profile = ProfileReport(df, title="EDA Report")
+
+# Save report
+profile.to_file("report.html")
+
+print("Report generated successfully!")
