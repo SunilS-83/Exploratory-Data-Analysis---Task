9e30ffd1c663cdfafa8fd6396739bb9694f2753f
SUNIL S <sunilsunil4277@gmail.com>
2026-05-04 10:47:31 +0530

    Reformat EDA task code for clarity

--- eda_task.py
+++ eda_task.py
@@ -2,7 +2,7 @@ import pandas as pd
 from data_profiling import ProfileReport
 
 # Load dataset
-df = pd.read_csv("data/Titanic-Dataset.csv")
+df = pd.read_csv("Titanic-Dataset.csv")
 
 # Generate report
 profile = ProfileReport(df, title="EDA Report")
